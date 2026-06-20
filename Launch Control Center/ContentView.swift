68882abcb059ff943d0a4318b0688ee7e1c4cc1e
Chris Werner <chris.werner@lunartelephone.com>
2026-06-20 09:24:13 -0700

    Initial Commit

---
+++ Launch Control Center/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Launch Control Center
+//
+//  Created by Chris Werner on 6/20/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Launch Control Center/Launch_Control_CenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Launch_Control_CenterApp.swift
+//  Launch Control Center
+//
+//  Created by Chris Werner on 6/20/2026.
+//
+
+import SwiftUI
+
+@main
+struct Launch_Control_CenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
